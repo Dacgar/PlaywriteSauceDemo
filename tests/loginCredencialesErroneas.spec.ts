803cdb3ff8aef184dcb4634356ea3d2c88e927d7
Diego Cadena <diecadman@hotmail.com>
2026-06-20 18:25:11 -0500

    Reto 2 completado

--- tests/loginCredencialesErroneas.spec.ts
+++ tests/loginCredencialesErroneas.spec.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@playwright/test";
 
-test('login to hrm incorrect', async({page}) =>{
+test('login to hrm incorrect', async ({ page }) => {
 
     await page.goto('https://opensource-demo.orangehrmlive.com')
-    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
-    await page.getByRole('textbox', {name:'Password'}).fill('admin1234')
-    await page.getByRole('button',{name:'Login'}).click()
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin1234')
+    await page.getByRole('button', { name: 'Login' }).click()
 
     const alert = page.getByRole('alert');
 
