41fd307e02004ee9b5f521c8fecc12f81be4b736
Diego Cadena <diecadman@hotmail.com>
2026-06-24 11:53:42 -0500

    reto Navigation menu

--- tests/Navigation.spec.ts
+++ tests/Navigation.spec.ts
@@ -44,3 +44,31 @@ test('Check left menu options', async({page}) => {
     expect(currentMenuItems).toEqual(expectedMenuItems)
 
 })
+
+test('Navigate through the left panel', async({page})=>{
+    test.setTimeout(60000);
+    await page.goto('https://opensource-demo.orangehrmlive.com')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount = await leftMenuItems.count()
+
+    for (let i=0; i<currentMenuItemsCount; i++){
+        const menuItem = leftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+
+        console.log('Current menu item',menuText)
+
+        await menuItem.click()
+
+        if (menuText === 'Maintenance'){
+            //await menuItem.click()
+            await page.goBack()
+            continue
+        }
+    }
+})
