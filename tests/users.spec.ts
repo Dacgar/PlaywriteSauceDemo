42f003641dfc1dd97164192635d95e3fd6d2fc5a
Diego Cadena <diecadman@hotmail.com>
2026-06-30 17:58:00 -0500

    Ejercicio clase 6

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -33,3 +33,46 @@ test('get all the usernames registered', async ({ page }) => {
     }
     console.log(usernames)
 })
+
+test('Select specific user for editions', async ({ page }) => {
+
+    const userForEdition = 'UserTest'
+
+    await page.goto('https://opensource-demo.orangehrmlive.com')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    await page.getByRole('link', { name: 'Admin' }).click();
+
+
+    //navegar y buscar opción por texto
+    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+    await page.getByRole('menuitem', { name: 'Users' }).click()
+
+    const pencilToEdit = page
+    //obtiene la tabla
+    .getByRole('table')
+    //obtiene los rows
+    .getByRole('row')
+    //obtiene el row que tenga el texto
+    .filter({hasText:userForEdition})
+    //devuelve los elementos del row
+    .locator('button')
+    //Devuelve el que tenga dentro del atributo la clase
+    .filter({has: page.locator('i.bi-pencil-fill')})
+
+    await pencilToEdit.click()
+    
+    const currentUserName = await page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")
+    .inputValue()
+    //Asersion 1
+    expect(currentUserName).toEqual(userForEdition)
+
+    //Asersion 2
+    //expect(page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input"))
+    //.toHaveValue(currentUserName)
+
+})
